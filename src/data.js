72e8c8bf9457861458dcd86db85a0545b482d2f1
ebeshero <ebbondar@gmail.com>
2023-11-13 14:55:34 -0500

    applying react-import-folder for gh-pages

--- src/data.js
+++ src/data.js
@@ -1,67 +1,81 @@
+import _Merton_College_library_hall from './assets/Merton_College_library_hall.jpg'
+import _Whale_VasconcelosBiblio_MexicoCity from './assets/Whale-VasconcelosBiblio-MexicoCity.jpg'
+import _bagpipe from './assets/bagpipe.png'
+import _bd_lilley_library_interior_1 from './assets/bd-lilley-library-interior-1.jpg'
+import _circuitry_nightcafe from './assets/circuitry-nightcafe.jpg'
+import _codingtube from './assets/codingtube.jpg'
+import _cragsidSpark_cropped from './assets/cragsidSpark-cropped.jpg'
+import _jacquard_loom_card from './assets/jacquard-loom-card.jpg'
+import _langMetropolis from './assets/langMetropolis.jpg'
+import _puffin_habitat from './assets/puffin-habitat.jpg'
+import _puffin1 from './assets/puffin1.jpg'
+import _puffin2_outsidehomes from './assets/puffin2-outsidehomes.jpg'
+import _spiritedAwayNightScene from './assets/spiritedAwayNightScene.jpg'
+import _warofworldswordcloud from './assets/warofworldswordcloud.png'
 export const galleryData = [{
     id: 0,
     name: 'bagpipes',
     kind: 'music',
     note: 'I like bagpipes! But I don\'t play them very well',
-    src: './assets/bagpipe.png'
+    src: _bagpipe
 }, {
     id: 1,
     name: 'Lilley Library at Penn State Behrend',
     kind: 'library',
     note: 'a library with beautiful sky lighting',
-    src: './assets/bd-lilley-library-interior-1.jpg'
+    src: _bd_lilley_library_interior_1
 }, {
     id: 2,
     name: 'Circuitry',
     kind: 'electric art',
     note: 'This was AI art I generated from prompts in Nightcafe',
-    src: './assets/circuitry-nightcafe.jpg'
+    src: _circuitry_nightcafe
 }, {
     id: 3,
     name: 'Coding tube',
     kind: 'electric art',
     note: 'A nifty 3D tube of code I found on the web',
-    src: './assets/codingtube.jpg'
+    src: _codingtube
 }, {
     id: 4,
     name: 'Spark form CragSide House, Morpeth UK',
     kind: 'electric art',
     note: 'Photo of an electrical spark from William Armstrong\'s experiments at Cragside in the 19th century',
-    src: './assets/cragsidSpark-cropped.jpg'
+    src: _cragsidSpark_cropped
 }, {
     id: 5,
     name: 'Jacquard loom card',
     kind: 'electric art',
     note: 'Card from an old Jacquard loom used for weaving complex patterns; a progenitor to computer punch cards',
-    src: './assets/jacquard-loom-card.jpg'
+    src: _jacquard_loom_card
 }, {
     id: 6,
     name: 'Whale at Vasconcelos Bibliotecha',
     kind: 'library',
     note: 'An enormous whale skeleton mounted on the ceiling of the Vasoncelos Library in Mexico City',
-    src: './assets/Whale-VasconcelosBiblio-MexicoCity.jpg'
+    src: _Whale_VasconcelosBiblio_MexicoCity
 }, {
     id: 7,
     name: 'Merton College Library',
     kind: 'library',
     note: 'An old chain library in England',
-    src: './assets/Merton_College_library_hall.jpg'
+    src: _Merton_College_library_hall
 }, {
     id: 8,
     name: 'A puffin',
     kind: 'bird',
     note: 'Here\'s a puffin I met at Borgarfjörður eystrioff the northeast coast of Iceland',
-    src: './assets/puffin1.jpg'
+    src: _puffin1
 }, {
     id: 9,
     name: 'Multiple puffins',
     kind: 'bird',
     note: 'Puffins outside their homes at Borgarfjörður eystri',
-    src: './assets/puffin2-outsidehomes.jpg'
+    src: _puffin2_outsidehomes
 }, {
     id: 10,
     name: 'Puffin habitat',
     kind: 'bird',
     note: 'A habitat for puffins at Borgarfjörður eystri',
-    src: './assets/puffin-habitat.jpg'
+    src: _puffin_habitat
 },];
---
+++ src/imgImports.js
@@ -0,0 +1,48 @@
+import _Merton_College_library_hall from './assets/Merton_College_library_hall.jpg'
+import _Whale_VasconcelosBiblio_MexicoCity from './assets/Whale-VasconcelosBiblio-MexicoCity.jpg'
+import _bagpipe from './assets/bagpipe.png'
+import _bd_lilley_library_interior_1 from './assets/bd-lilley-library-interior-1.jpg'
+import _circuitry_nightcafe from './assets/circuitry-nightcafe.jpg'
+import _codingtube from './assets/codingtube.jpg'
+import _cragsidSpark_cropped from './assets/cragsidSpark-cropped.jpg'
+import _jacquard_loom_card from './assets/jacquard-loom-card.jpg'
+import _langMetropolis from './assets/langMetropolis.jpg'
+import _puffin_habitat from './assets/puffin-habitat.jpg'
+import _puffin1 from './assets/puffin1.jpg'
+import _puffin2_outsidehomes from './assets/puffin2-outsidehomes.jpg'
+import _spiritedAwayNightScene from './assets/spiritedAwayNightScene.jpg'
+import _warofworldswordcloud from './assets/warofworldswordcloud.png'
+
+
+const data = [
+{img: _Merton_College_library_hall,
+ name: "Merton_College_library_hall", folder: "./assets"},
+{img: _Whale_VasconcelosBiblio_MexicoCity,
+ name: "Whale-VasconcelosBiblio-MexicoCity", folder: "./assets"},
+{img: _bagpipe,
+ name: "bagpipe", folder: "./assets"},
+{img: _bd_lilley_library_interior_1,
+ name: "bd-lilley-library-interior-1", folder: "./assets"},
+{img: _circuitry_nightcafe,
+ name: "circuitry-nightcafe", folder: "./assets"},
+{img: _codingtube,
+ name: "codingtube", folder: "./assets"},
+{img: _cragsidSpark_cropped,
+ name: "cragsidSpark-cropped", folder: "./assets"},
+{img: _jacquard_loom_card,
+ name: "jacquard-loom-card", folder: "./assets"},
+{img: _langMetropolis,
+ name: "langMetropolis", folder: "./assets"},
+{img: _puffin_habitat,
+ name: "puffin-habitat", folder: "./assets"},
+{img: _puffin1,
+ name: "puffin1", folder: "./assets"},
+{img: _puffin2_outsidehomes,
+ name: "puffin2-outsidehomes", folder: "./assets"},
+{img: _spiritedAwayNightScene,
+ name: "spiritedAwayNightScene", folder: "./assets"},
+{img: _warofworldswordcloud,
+ name: "warofworldswordcloud", folder: "./assets"},
+
+]
+export default data;
