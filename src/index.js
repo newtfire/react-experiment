710510523793b202bc3fc27fc3be1b963e4a03ba
ebeshero <ebbondar@gmail.com>
2023-12-06 02:46:19 -0500

    splicing repo name to public url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter baseName={process.env.PUBLIC_URL}>
+    <BrowserRouter baseName={process.env.PUBLIC_URL || '/react-experiment/'}>
+
     <App />
     </BrowserRouter>
 );
